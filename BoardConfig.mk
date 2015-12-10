6f55827194ea5b2e07ad78873f6e01ffd5ebbe21
SdtBarbarossa <DaMicha_essen@gmx.de>
2015-12-10 15:06:41 +0100

    Update BoardConfig.mk
    - adding comments for more readable code
    - TW_NO_USB_STORAGE := true   <<< does this device dont have any usb connector? why disable mtp?

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,8 +3,7 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/huawei/p8litekirin/BoardConfigVendor.mk
 
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := kirin930
+# Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
@@ -18,28 +17,44 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 
+# Board
+TARGET_NO_BOOTLOADER := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_BOARD_PLATFORM := kirin930
 TARGET_BOOTLOADER_BOARD_NAME := p8litekirin
 
+# Kernel
 BOARD_KERNEL_CMDLINE := hisi_dma_print=0 vmalloc=384M maxcpus=8 no_irq_affinity androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x07478000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x07b88000 --tags_offset 0x02988000
 
+# Kernelconfig
+# Use this for prebuild
+TARGET_PREBUILT_KERNEL := device/huawei/p8litekirin/kernel
+# Use this for build from source (need declares)
+# TARGET_KERNEL_SOURCE := kernel/huawei
+# TARGET_KERNEL_CONFIG := aosp_p8litekirin_defconfig
+
+# Partitionsizes
 BOARD_BOOTIMAGE_PARTITION_SIZE := 25165824
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 11605639168
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-TARGET_PREBUILT_KERNEL := device/huawei/p8litekirin/kernel
-
-BOARD_HAS_NO_SELECT_BUTTON := true
+# Recovery Specific flags
+# DEVICE_RESOLUTION is depricated! try TW_THEME instead
 DEVICE_RESOLUTION := 720x1280
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 RECOVERY_SDCARD_ON_DATA := true
-TW_NO_USB_STORAGE := true
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_HAS_LARGE_FILESYSTEM := true
+
+# TWRP specific flags
+TW_NO_USB_STORAGE := true
+# if you dont want DEVICE_RESOLUTION use this
+# TW_THEME := portrait_mdpi
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd_backlight0/brightness"
 TW_CUSTOM_BATTERY_PATH := "/sys/devices/platform/bq_bci_battery.1/power_supply/Battery"
 TW_MAX_BRIGHTNESS := 255
